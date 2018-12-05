7b56dd8c34b3b120cfcab232e66eab285cadc1d2
刘卫星 <weixing.liu@tigermed.net>
2018-12-05 14:43:11 +0800

    Initial Commit

---
+++ ConreRadius/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ConreRadius
+//
+//  Created by wahaha on 2018/12/5.
+//  Copyright © 2018年 LWX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ ConreRadius/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConreRadius
+//
+//  Created by wahaha on 2018/12/5.
+//  Copyright © 2018年 LWX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
